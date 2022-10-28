29737824168bf0635dbe7539ac76ec86f0db7d2e
Antoine Daneau <twan472@hotmail.com>
2022-10-28 09:55:48 -0400

    commit initial

---
+++ src/main/java/net/daneau/assnat/AssnatApplication.java
@@ -0,0 +1,13 @@
+package net.daneau.assnat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssnatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssnatApplication.class, args);
+    }
+
+}
