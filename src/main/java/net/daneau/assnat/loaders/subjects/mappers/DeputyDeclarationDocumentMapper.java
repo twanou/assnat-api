3b30632f3bdee6b902bc6071428cf4515d33a96d
Antoine Daneau <twan472@hotmail.com>
2023-11-18 21:32:46 -0500

    wip 1

--- src/main/java/net/daneau/assnat/loaders/subjects/mappers/DeputyDeclarationDocumentMapper.java
+++ src/main/java/net/daneau/assnat/loaders/subjects/mappers/DeputyDeclarationDocumentMapper.java
@@ -1,12 +1,7 @@
 package net.daneau.assnat.loaders.subjects.mappers;
 
-import lombok.RequiredArgsConstructor;
-import net.daneau.assnat.client.documents.subdocuments.Assignment;
-import net.daneau.assnat.client.documents.subdocuments.InterventionDocument;
-import net.daneau.assnat.client.documents.subdocuments.SubjectDetails;
 import net.daneau.assnat.client.documents.subdocuments.SubjectType;
 import net.daneau.assnat.loaders.DeputyFinder;
-import net.daneau.assnat.scrappers.models.ScrapedLogNode;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
@@ -14,28 +9,12 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 @Component
-@RequiredArgsConstructor
-public class DeputyDeclarationDocumentMapper implements SubjectDocumentTypeMapper {
-    private final DeputyFinder deputyFinder;
+public class DeputyDeclarationDocumentMapper extends SubjectDocumentTypeMapper {
 
     private static final String VICE_PRESIDENT = "vice-président";
 
-    @Override
-    public List<SubjectDetails> map(ScrapedLogNode logNode) {
-        return logNode.getChildren().stream().map(declaration -> {
-            Assignment assignment = this.deputyFinder.findByCompleteName(declaration.getChildren().get(0).getTitle()); // nom complet, ex M. Bob Tremblay
-            return SubjectDetails.builder()
-                    .type(SubjectType.DEPUTY_DECLARATION)
-                    .title(declaration.getTitle())
-                    .interventions(List.of(
-                            InterventionDocument.builder()
-                                    .deputyId(assignment.getDeputyId())
-                                    .partyId(assignment.getPartyId())
-                                    .districtId(assignment.getDistrictId())
-                                    .paragraphs(this.cleanParagraphs(declaration.getChildren().get(0).getParagraphs()))
-                                    .build()))
-                    .build();
-        }).toList();
+    public DeputyDeclarationDocumentMapper(DeputyFinder deputyFinder) {
+        super(deputyFinder);
     }
 
     @Override
@@ -43,10 +22,16 @@ public class DeputyDeclarationDocumentMapper implements SubjectDocumentTypeMappe
         return List.of(AFFAIRES_COURANTES, DECLARATIONS_DE_DEPUTES);
     }
 
-    private List<String> cleanParagraphs(List<String> paragraphs) {
+    @Override
+    protected List<String> format(List<String> paragraphs) {
         return IntStream.range(0, this.getEndRange(paragraphs)) // On ignore à partir du moment que le VP se met à parler
                 .mapToObj(i -> i == 0 ? this.removeDeputyName(paragraphs.get(i)) : paragraphs.get(i)) // retrait du nom du député au début du premier paragraphe
                 .toList();
+    }
+
+    @Override
+    protected SubjectType getSubjectType() {
+        return SubjectType.DEPUTY_DECLARATION;
     }
 
     private int getEndRange(List<String> paragraphs) {
--- src/main/java/net/daneau/assnat/loaders/subjects/mappers/QuestionsAnswersDocumentMapper.java
+++ src/main/java/net/daneau/assnat/loaders/subjects/mappers/QuestionsAnswersDocumentMapper.java
@@ -1,45 +1,26 @@
 package net.daneau.assnat.loaders.subjects.mappers;
 
-import lombok.RequiredArgsConstructor;
-import net.daneau.assnat.client.documents.subdocuments.Assignment;
-import net.daneau.assnat.client.documents.subdocuments.InterventionDocument;
-import net.daneau.assnat.client.documents.subdocuments.SubjectDetails;
 import net.daneau.assnat.client.documents.subdocuments.SubjectType;
 import net.daneau.assnat.loaders.DeputyFinder;
-import net.daneau.assnat.scrappers.models.ScrapedLogNode;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
-public class QuestionsAnswersDocumentMapper implements SubjectDocumentTypeMapper {
+public class QuestionsAnswersDocumentMapper extends SubjectDocumentTypeMapper {
 
-    private final DeputyFinder deputyFinder;
+    public QuestionsAnswersDocumentMapper(DeputyFinder deputyFinder) {
+        super(deputyFinder);
+    }
 
     @Override
-    public List<SubjectDetails> map(ScrapedLogNode logNode) {
-        return logNode.getChildren()
-                .stream()
-                .map(qaNode -> {
-                    List<InterventionDocument> interventionDocuments = qaNode.getChildren()
-                            .stream()
-                            .map(intervention -> {
-                                Assignment assignment = this.deputyFinder.findByCompleteName(intervention.getTitle()); // nom complet, ex M. Bob Tremblay
-                                return InterventionDocument.builder()
-                                        .deputyId(assignment.getDeputyId())
-                                        .partyId(assignment.getPartyId())
-                                        .districtId(assignment.getDistrictId())
-                                        .paragraphs(intervention.getParagraphs())
-                                        .build();
-                            }).toList();
+    protected List<String> format(List<String> paragraphs) {
+        return paragraphs;
+    }
 
-                    return SubjectDetails.builder()
-                            .type(SubjectType.QUESTIONS_ANSWERS)
-                            .title(qaNode.getTitle())
-                            .interventions(interventionDocuments)
-                            .build();
-                }).toList();
+    @Override
+    protected SubjectType getSubjectType() {
+        return SubjectType.QUESTIONS_ANSWERS;
     }
 
     @Override
