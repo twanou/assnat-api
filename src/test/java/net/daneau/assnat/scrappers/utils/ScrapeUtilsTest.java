fa074c3f1cb1f15be2f5bc17b45a905134426b4f
twanou <twan472@hotmail.com>
2023-11-24 15:34:22 -0500

    Correctifs en tout genre.

--- src/test/java/net/daneau/assnat/scrappers/utils/ScrapeUtilsTest.java
+++ src/test/java/net/daneau/assnat/scrappers/utils/ScrapeUtilsTest.java
@@ -8,10 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ScrapeUtilsTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"1234.56cm", "1234.56pt"})
-    void onlyDigitsToInt(String value) {
-        int result = ScrapeUtils.onlyDigitsToInt(value);
-        assertEquals(1234.56, result);
+    @ValueSource(strings = {"abcdefg1234hijklmnop56", "1234.56pt"})
+    void onlyDigitsToInt() {
+        int result = ScrapeUtils.onlyDigitsToInt("abcdefg1234hijklmnop56");
+        assertEquals(123456, result);
     }
 
     @ParameterizedTest
