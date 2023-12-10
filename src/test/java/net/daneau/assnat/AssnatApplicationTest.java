4c7013767168064a34d8ad81157ff0e675d8ef65
Antoine Daneau <twan472@hotmail.com>
2023-12-10 13:37:38 -0500

    Ajout d'un profile spring pour le "loader".

--- src/test/java/net/daneau/assnat/AssnatApplicationTest.java
+++ src/test/java/net/daneau/assnat/AssnatApplicationTest.java
@@ -3,11 +3,9 @@ package net.daneau.assnat;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @Disabled
 @SpringBootTest
-@ActiveProfiles("test")
 class AssnatApplicationTest {
 
     @Test
