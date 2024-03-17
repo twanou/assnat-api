6d461a4388b47da05fda3a7b05383369c6225426
twanou <twan472@hotmail.com>
2024-03-17 14:49:46 -0400

    dsfsdfsd

--- src/main/java/quebec/salonbleu/assnat/loaders/assignments/AssignmentLoader.java
+++ src/main/java/quebec/salonbleu/assnat/loaders/assignments/AssignmentLoader.java
@@ -9,21 +9,16 @@ import quebec.salonbleu.assnat.client.documents.Deputy;
 import quebec.salonbleu.assnat.client.documents.District;
 import quebec.salonbleu.assnat.client.documents.Party;
 import quebec.salonbleu.assnat.client.repositories.AssignmentRepository;
-import quebec.salonbleu.assnat.loaders.exceptions.LoadingException;
 import quebec.salonbleu.assnat.scrapers.DeputyScraper;
 import quebec.salonbleu.assnat.scrapers.models.ScrapedDeputy;
-import quebec.salonbleu.assnat.utils.ErrorHandler;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class AssignmentLoader {
 
-    private final ErrorHandler errorHandler;
     private final DeputyScraper deputyScraper;
     private final DeputyLoader deputyLoader;
     private final PartyLoader partyLoader;
@@ -33,7 +28,6 @@ public class AssignmentLoader {
 
     public void load() {
         List<ScrapedDeputy> scrapedDeputies = this.deputyScraper.scrape();
-        this.assertNoDuplicateScrapedDeputies(scrapedDeputies);
 
         List<Assignment> currentAssignments = this.assignmentRepository.findByEndDate(null);
         List<ScrapedDeputy> newScrapedDeputies = this.getScrapedDeputiesNotInAssignment(scrapedDeputies, currentAssignments);
@@ -76,26 +70,25 @@ public class AssignmentLoader {
 
     private List<ScrapedDeputy> getScrapedDeputiesNotInAssignment(List<ScrapedDeputy> scrapedDeputies, List<Assignment> assignments) {
         return scrapedDeputies.stream()
-                .filter(scrapedDeputy -> assignments
-                        .stream()
+                .filter(scrapedDeputy -> assignments.stream()
                         .noneMatch(assignment -> assignment.getHash() == scrapedDeputy.hashCode()))
                 .toList();
     }
 
     private List<Assignment> getAssignmentNotInScrapedDeputies(List<ScrapedDeputy> scrapedDeputies, List<Assignment> assignments) {
         return assignments.stream()
-                .filter(assignment -> scrapedDeputies
-                        .stream()
+                .filter(assignment -> scrapedDeputies.stream()
                         .noneMatch(scrapedDeputy -> scrapedDeputy.hashCode() == assignment.getHash()))
                 .toList();
     }
 
     private Deputy getDeputy(ScrapedDeputy scrapedDeputy, List<Deputy> deputies) {
-        List<Deputy> deputyResults = deputies.stream()
+        return deputies.stream()
                 .filter(d -> StringUtils.equals(d.getFirstName(), scrapedDeputy.getFirstName()))
                 .filter(d -> StringUtils.equals(d.getLastName(), scrapedDeputy.getLastName()))
-                .toList();
-        return deputyResults.get(0);
+                .filter(d -> StringUtils.equals(d.getLastDistrict(), scrapedDeputy.getDistrict()))
+                .findFirst()
+                .orElseThrow();
     }
 
     private District getDistrict(ScrapedDeputy scrapedDeputy, List<District> districts) {
@@ -110,15 +103,5 @@ public class AssignmentLoader {
                 .filter(p -> StringUtils.equals(p.getName(), scrapedDeputy.getParty()))
                 .findFirst()
                 .orElseThrow();
-    }
-
-    private void assertNoDuplicateScrapedDeputies(List<ScrapedDeputy> scrapedDeputies) {
-        Set<ScrapedDeputy> elements = new HashSet<>();
-        List<ScrapedDeputy> results = scrapedDeputies.stream()
-                .filter(n -> !elements.add(n))
-                .toList();
-
-        //On verra ce qu'on fait si ça arrive!
-        this.errorHandler.assertSize(0, results, () -> new LoadingException("Deux députés avec le même nom."));
     }
 }
--- src/test/java/quebec/salonbleu/assnat/loaders/assignments/AssignmentLoaderTest.java
+++ src/test/java/quebec/salonbleu/assnat/loaders/assignments/AssignmentLoaderTest.java
@@ -13,18 +13,14 @@ import quebec.salonbleu.assnat.client.documents.Deputy;
 import quebec.salonbleu.assnat.client.documents.District;
 import quebec.salonbleu.assnat.client.documents.Party;
 import quebec.salonbleu.assnat.client.repositories.AssignmentRepository;
-import quebec.salonbleu.assnat.loaders.exceptions.LoadingException;
 import quebec.salonbleu.assnat.scrapers.DeputyScraper;
 import quebec.salonbleu.assnat.scrapers.models.ScrapedDeputy;
-import quebec.salonbleu.assnat.utils.ErrorHandler;
 import test.utils.TestUUID;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atMostOnce;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -33,8 +29,6 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class AssignmentLoaderTest {
 
-    @Mock
-    private ErrorHandler errorHandlerMock;
     @Mock
     private DeputyScraper deputyScraperMock;
     @Mock
@@ -54,7 +48,7 @@ class AssignmentLoaderTest {
     void load() {
         List<ScrapedDeputy> scrapedDeputies = List.of(ScrapedDeputy.builder().firstName("Bernard").lastName("Landry").party("Parti Québecois").district("Verchères").build(), ScrapedDeputy.builder().firstName("René").build());
         List<Assignment> currentAssignments = List.of(Assignment.builder().hash(1733353033).build(), Assignment.builder().hash(1995).build());
-        Deputy landryDeputy = Deputy.builder().id(TestUUID.ID1).firstName("Bernard").lastName("Landry").build();
+        Deputy landryDeputy = Deputy.builder().id(TestUUID.ID1).firstName("Bernard").lastName("Landry").lastDistrict("Verchères").build();
         District landryDistrict = District.builder().id(TestUUID.ID2).name("Verchères").build();
         Party landryParty = Party.builder().id(TestUUID.ID3).name("Parti Québecois").build();
         when(deputyScraperMock.scrape()).thenReturn(scrapedDeputies);
@@ -77,13 +71,12 @@ class AssignmentLoaderTest {
                 .endDate(LocalDate.now())
                 .build());
         order.verify(assnatCacheManagerMock).clearAllCaches();
-        verify(errorHandlerMock).assertSize(eq(0), eq(List.of()), argThat(s -> s.get() instanceof LoadingException));
     }
 
     @Test
     void loadWithoutCurrentAssignments() {
         List<ScrapedDeputy> scrapedDeputies = List.of(ScrapedDeputy.builder().firstName("Bernard").lastName("Landry").party("Parti Québecois").district("Verchères").build());
-        Deputy landryDeputy = Deputy.builder().id(TestUUID.ID1).firstName("Bernard").lastName("Landry").build();
+        Deputy landryDeputy = Deputy.builder().id(TestUUID.ID1).firstName("Bernard").lastName("Landry").lastDistrict("Verchères").build();
         District landryDistrict = District.builder().id(TestUUID.ID2).name("Verchères").build();
         Party landryParty = Party.builder().id(TestUUID.ID3).name("Parti Québecois").build();
         when(deputyScraperMock.scrape()).thenReturn(scrapedDeputies);
@@ -103,7 +96,6 @@ class AssignmentLoaderTest {
                 .build());
         order.verify(assnatCacheManagerMock).clearAllCaches();
         verify(assignmentRepositoryMock, atMostOnce()).save(any(Assignment.class));
-        verify(errorHandlerMock).assertSize(eq(0), eq(List.of()), argThat(s -> s.get() instanceof LoadingException));
     }
 
     @Test
@@ -116,21 +108,5 @@ class AssignmentLoaderTest {
         this.assignmentLoader.load();
         verify(assnatCacheManagerMock, never()).clearAllCaches();
         verify(assignmentRepositoryMock, never()).save(any(Assignment.class));
-        verify(errorHandlerMock).assertSize(eq(0), eq(List.of()), argThat(s -> s.get() instanceof LoadingException));
-    }
-
-    @Test
-    void assertDuplicate() {
-        ScrapedDeputy rene = ScrapedDeputy.builder().firstName("René").lastName("Lévesque").build();
-        List<ScrapedDeputy> scrapedDeputies = List.of(
-                rene,
-                ScrapedDeputy.builder().firstName("Jacques").lastName("Parizeau").build(),
-                ScrapedDeputy.builder().firstName("Lucien").lastName("Bouchard").build(),
-                rene
-        );
-        when(deputyScraperMock.scrape()).thenReturn(scrapedDeputies);
-
-        this.assignmentLoader.load();
-        verify(errorHandlerMock).assertSize(eq(0), eq(List.of(rene)), argThat(s -> s.get() instanceof LoadingException));
     }
 }
