d6b10074a15186326c158a72217fdc5e331761ff
twanou <github@twanou.dev>
2024-05-26 17:04:29 -0400

    Modification du deputyScraper pour obtenir le titre, les fonctions et la photo du député.

--- src/main/java/net/daneau/assnat/scrappers/DeputyScraper.java
+++ src/main/java/net/daneau/assnat/scrappers/DeputyScraper.java
@@ -1,25 +1,32 @@
 package net.daneau.assnat.scrappers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.daneau.assnat.scrappers.configuration.AssNatWebClient;
 import net.daneau.assnat.scrappers.exceptions.ScrapingException;
 import net.daneau.assnat.scrappers.models.ScrapedDeputy;
 import net.daneau.assnat.utils.ErrorHandler;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.htmlunit.html.DomElement;
+import org.htmlunit.html.DomNode;
 import org.htmlunit.html.HtmlAnchor;
 import org.htmlunit.html.HtmlImage;
+import org.htmlunit.html.HtmlListItem;
 import org.htmlunit.html.HtmlPage;
 import org.htmlunit.html.HtmlTableDataCell;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DeputyScraper {
@@ -31,23 +38,32 @@ public class DeputyScraper {
 
     private static final String INDEPENDENT = "Indépendant";
     private static final String INDEPENDENT_PARTY = "Indépendant(e)";
+    private static final String DEPUTEE = "Députée";
+    private static final String M = "M.";
+    private static final String MME = "Mme";
 
     public List<ScrapedDeputy> scrape() {
+        log.info("Début scraping députés");
         HtmlPage page = this.webClient.getRelativePage(relativeUrl);
         List<DomElement> rows = page.getByXPath("//table[@id='ListeDeputes']//tbody//tr");
         List<ScrapedDeputy> deputies = new ArrayList<>();
         for (DomElement row : rows) {
             List<HtmlTableDataCell> cells = row.getByXPath(".//td");
+            HtmlPage deputyPage = this.getDeputyPage(cells.get(0));
+            List<String> functions = this.getFunctions(deputyPage);
             deputies.add(
                     ScrapedDeputy.builder()
+                            .title(this.getTitle(functions))
                             .firstName(StringUtils.strip(cells.get(0).getVisibleText().split(",")[1]))
                             .lastName(StringUtils.strip(cells.get(0).getVisibleText().split(",")[0]))
                             .district(cells.get(1).getVisibleText())
                             .party(this.independenceCheck(cells.get(2).getVisibleText()))
-                            //.image(this.getImage(cells.get(0)))
+                            .functions(functions)
+                            .photo(this.getPhotoBase64(deputyPage))
                             .build()
             );
         }
+        log.info("Fin scraping députés");
         this.errorHandler.assertSize(rows.size(), deputies, () -> new ScrapingException("Nombre de député invalide"));
         deputies.sort(Comparator.comparing(deputy -> Normalizer.normalize(deputy.getLastName() + deputy.getFirstName(), Normalizer.Form.NFD)));
         return Collections.unmodifiableList(deputies);
@@ -57,9 +73,30 @@ public class DeputyScraper {
         return StringUtils.containsIgnoreCase(party, INDEPENDENT) ? INDEPENDENT_PARTY : party;
     }
 
-    private HtmlImage getImage(HtmlTableDataCell cell) {
+    private HtmlPage getDeputyPage(HtmlTableDataCell cell) {
         HtmlAnchor anchor = cell.getFirstByXPath(".//a");
-        HtmlPage deputyPage = this.webClient.getRelativePage(anchor.getHrefAttribute());
-        return deputyPage.getFirstByXPath("//img[@class='photoDepute']");
+        return this.webClient.getRelativePage(anchor.getHrefAttribute());
+    }
+
+    private String getTitle(List<String> functions) {
+        return functions.stream()
+                .anyMatch(function -> function.contains(DEPUTEE)) ? MME : M;
+    }
+
+    private List<String> getFunctions(HtmlPage deputyPage) {
+        List<HtmlListItem> functionItmes = deputyPage.getByXPath("//div[@class='enteteFicheDepute']//ul//li");
+        return functionItmes.stream()
+                .map(DomNode::getVisibleText)
+                .toList();
+    }
+
+    private String getPhotoBase64(HtmlPage deputyPage) {
+        HtmlImage htmlImage = deputyPage.getFirstByXPath("//img[@class='photoDepute']");
+        try {
+            InputStream deputyImageStream = htmlImage.getWebResponse(true).getContentAsStream();
+            return Base64.getEncoder().encodeToString(IOUtils.toByteArray(deputyImageStream));
+        } catch (Exception e) {
+            throw new ScrapingException("Impossible d'extraire la photo", e);
+        }
     }
 }
