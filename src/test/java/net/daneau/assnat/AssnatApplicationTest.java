8d151a65e0b8f199a29eb44572aebf936fc53992
twanou <github@twanou.dev>
2024-05-26 17:04:24 -0400

    commit initial

---
+++ src/test/java/net/daneau/assnat/AssnatApplicationTest.java
@@ -0,0 +1,15 @@
+package net.daneau.assnat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AssnatApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
